d79828394db92899b3f3f304ffb9cd5ca7f2547f
Amante Reale <amantereale@gmail.com>
2017-02-27 22:40:36 +0100

    Implemented stateful search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return (
-    <input />
-  );
-};
+class SearchBar extends Component { //extends React.Component
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange.bind(this)} />
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
