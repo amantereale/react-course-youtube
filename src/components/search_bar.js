821f76980e29ceb849b86a6c9457404b61a929d4
Amante Reale <amantereale@gmail.com>
2017-02-28 14:17:21 +0100

    started video list; implemented YTSearch

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component { //extends React.Component
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { term: '' };
-  }
+        this.state = {
+            term: ''
+        };
+    }
 
-  onInputChange(event) {
-    this.setState({term: event.target.value});
-  }
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+    }
 
-  render() {
-    return (
-      <div>
-        <input
-          value={this.state.term}
-          onChange={this.onInputChange.bind(this)} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <input value={this.state.term} onChange={this.onInputChange.bind(this)}/>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = ' AIzaSyDUWDKy6CjY0clDj-GsSoIXZQ_0q53WyjY';
+const API_KEY = 'AIzaSyDUWDKy6CjY0clDj-GsSoIXZQ_0q53WyjY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'puppies'
+        }, (videos) => {
+            this.setState({videos}); //same as this.setState({ videos: videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <App/>, document.querySelector('.container'));
