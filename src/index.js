ea4d958432c0f55d11eeb1520c36aaafec01f113
Amante Reale <amantereale@gmail.com>
2017-02-28 18:24:37 +0100

    Fixed issue with video id; added video detail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDUWDKy6CjY0clDj-GsSoIXZQ_0q53WyjY';
 
@@ -26,6 +28,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos}/>
             </div>
         );
